8c89199534b9213913a21adb0a930dc85e2be5e0
Gamer1120 <Gamer1120@github.com>
2022-05-02 13:15:23 +0200

    Add TG sending and don't resend banners

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
+import time, requests, json, os
 
-import requests, json
+FILENAME = "known-offline-banners.txt"
+TELEGRAM_ACCESS_TOKEN = os.environ.get('TELEGRAM_ACCESS_TOKEN', None)
+CHAT_ID = '@bbonldata'
 
 def getAllNLBanners():
     banner_array = []
@@ -17,7 +19,33 @@ def getAllNLBanners():
         time.sleep(0.5)
     return banner_array
 
+def getAllKnownOfflineBanners():
+    knownBanners = []
+    if os.path.exists(FILENAME):
+        with open(FILENAME, 'r') as file:
+            lines = file.readlines()
+            knownBanners = [line.rstrip() for line in lines]
+    return knownBanners
+
+def addKnownOfflineBanner(id):
+    with open(FILENAME, 'a') as file:
+        file.write(id + '\n')
+
+api_url = 'https://api.telegram.org/bot{token}/{method}'.format
+
+def telegram_command(name, data):
+    url = api_url(token=TELEGRAM_ACCESS_TOKEN, method=name)
+    return requests.post(url=url, json=data)
+
+def telegram_sendMessage(text: str, chat_id: str, notify=True):
+    return telegram_command('sendMessage', {
+        'text': text,
+        'chat_id': chat_id,
+        'parse_mode': 'markdown',
+        'disable_notification': not notify})
+
 banners = getAllNLBanners()
+knownOfflineBanners = getAllKnownOfflineBanners()
 print(str(len(banners)) + " banners found. Now processing.")
 currentbanner = 0
 for banner in getAllNLBanners():
@@ -26,5 +54,10 @@ for banner in getAllNLBanners():
     currentbanner += 1
     results = requests.get('https://api.bannergress.com/bnrs/' + banner['id']).json()
     if 'plannedOfflineDate' in results:
-        print("https://bannergress.com/banner/" + results['id'] + " " + results['plannedOfflineDate'])
+        id = results['id']
+        if id not in knownOfflineBanners:
+            addKnownOfflineBanner(id)
+            convertedOfflineDate = results['plannedOfflineDate']
+            print(telegram_sendMessage('https://bannergress.com/banner/' + id + '\nOffline per ' + results['plannedOfflineDate'] + '.', '@bbonldata', True).content)
+            print("https://bannergress.com/banner/" + results['id'] + " " + results['plannedOfflineDate'])
     time.sleep(0.5)
